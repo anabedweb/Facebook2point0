05e5cf31e0e9fa224e3448a18d450212da4b4bf5
Anabel Bedlan <honeybedlan@icloud.com>
2024-12-09 16:37:55 -0800

    Initial Commit

---
+++ Wild Wild Western/Wild_Wild_WesternApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wild_Wild_WesternApp.swift
+//  Wild Wild Western
+//
+//  Created by Anabel Bedlan on 12/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Wild_Wild_WesternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Wild Wild WesternTests/Wild_Wild_WesternTests.swift
@@ -0,0 +1,17 @@
+//
+//  Wild_Wild_WesternTests.swift
+//  Wild Wild WesternTests
+//
+//  Created by Anabel Bedlan on 12/3/24.
+//
+
+import Testing
+@testable import Wild_Wild_Western
+
+struct Wild_Wild_WesternTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
